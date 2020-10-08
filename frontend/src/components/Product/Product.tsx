f8b069542e17b3870b534131d5cbfa8041b2010a
Van Darrell Ponce <vanponce91@gmail.com>
2020-10-08 18:06:06 +0800

    added initial product component

---
+++ frontend/src/components/Product/Product.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card'
+
+interface Props {
+	product: {
+		_id: string
+		name: string
+		image: string
+		description: string
+		brand: string
+		category: string
+		price: number
+		countInStock: number
+		rating: number
+		numReviews: number
+	}
+}
+
+const Product: React.FC<Props> = (props) => {
+	const { product } = props
+	return (
+		<Card className="my-3 p-3 rounded">
+			<a href={`/products/${product._id}`}>
+				<Card.Img src={product.image} variant="top" />
+			</a>
+
+			<Card.Body>
+				<a href={`/products/${product._id}`}>
+					<Card.Title as="div">
+						<strong>{product.name}</strong>
+					</Card.Title>
+				</a>
+
+				<Card.Text as="div">
+					<div className="my-3">
+						{product.rating} from {product.numReviews} reviews
+					</div>
+				</Card.Text>
+
+				<Card.Text as="h3"> &#8369;{product.price}</Card.Text>
+			</Card.Body>
+		</Card>
+	)
+}
+
+export default Product
--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Col from 'react-bootstrap/esm/Col'
 import Row from 'react-bootstrap/esm/Row'
+import Product from '../components/Product/Product'
 import products from '../products'
 
 const Home = () => {
@@ -9,7 +10,9 @@ const Home = () => {
 			<h2 className="text-center">Latest Products</h2>
 			<Row>
 				{products.map((product) => (
-					<Col sm={12} md={6} lg={4} xl={3} key={product.id}></Col>
+					<Col sm={12} md={6} lg={4} xl={3} key={product.id}>
+						<Product product={product} />
+					</Col>
 				))}
 			</Row>
 		</div>
