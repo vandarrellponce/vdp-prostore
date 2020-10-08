f1c95917f18b2699359347cb9ace6916e25829f5
Van Darrell Ponce <vanponce91@gmail.com>
2020-10-08 13:53:30 +0800

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './app.css'
+
+const App = () => {
+	return (
+		<div className="app">
+			<h1>welcome to my home</h1>
+			<h2></h2>
+		</div>
+	)
+}
+
+export default App
