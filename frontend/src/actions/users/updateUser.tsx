62ad741f68b144e10febb72db90164aa53ee82c9
Van Darrell Ponce <vanponce91@gmail.com>
2020-10-24 20:14:53 +0800

    update user by admin bug

--- frontend/src/actions/users/updateUser.tsx
+++ frontend/src/actions/users/updateUser.tsx
@@ -11,14 +11,8 @@ import {
 export const updateUser = (updatedUser) => async (dispatch) => {
 	try {
 		dispatch({ type: USER_UPDATE_REQUEST })
-		const config = {
-			headers: {
-				Authorization: `Bearer ${JSON.parse(localStorage.getItem('token'))}`,
-			},
-		}
-		const user = await (
-			await Axios.put(`/api/users/profile`, updatedUser, config)
-		).data
+
+		const user = await (await Axios.put(`/api/users/profile`, updatedUser)).data
 
 		dispatch({ type: USER_UPDATE_SUCCESS, payload: user })
 	} catch (error) {
@@ -34,18 +28,9 @@ export const updateUser = (updatedUser) => async (dispatch) => {
 export const updateUserByAdmin = (updatedUser, id) => async (dispatch) => {
 	try {
 		dispatch({ type: USER_DETAILS_REQUEST })
-		const config = {
-			headers: {
-				Authorization: `Bearer ${JSON.parse(localStorage.getItem('token'))}`,
-			},
-		}
-		const user = await (
-			await Axios.put(
-				`http://localhost:5000/api/admin/users/${id}`,
-				updatedUser,
-				config
-			)
-		).data
+
+		const user = await (await Axios.put(`/api/admin/users/${id}`, updatedUser))
+			.data
 
 		dispatch({ type: USER_DETAILS_SUCCESS, payload: user })
 	} catch (error) {
