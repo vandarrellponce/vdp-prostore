12d0d18e600f87637186034ca97e1db98093d6f8
Van Darrell Ponce <vanponce91@gmail.com>
2020-10-25 18:13:36 +0800

    additional layout

--- frontend/src/components/Product/Product.tsx
+++ frontend/src/components/Product/Product.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Card from 'react-bootstrap/Card'
 import Rating from '../Rating/Rating'
 import { Link } from 'react-router-dom'
+import './Product.css'
+import Loader from '../Loader/Loader'
 
 interface Props {
 	product: {
@@ -20,30 +22,33 @@ interface Props {
 
 const Product: React.FC<Props> = (props) => {
 	const { product } = props
-	return (
-		<Card className="my-1 p-1 rounded" style={{ fontSize: '15px' }}>
-			<Link to={`/products/${product._id}`}>
-				<Card.Img src={product.image} variant="top" />
-			</Link>
 
-			<Card.Body>
+	return (
+		<div className="mycontainer">
+			<Card className="my-1 p-1 rounded card" style={{ fontSize: '15px' }}>
 				<Link to={`/products/${product._id}`}>
-					<Card.Title as="div">
-						<strong>{product.name}</strong>
-					</Card.Title>
+					<Card.Img src={product.image} variant="top" />
 				</Link>
 
-				<Card.Text as="div">
-					<Rating
-						value={product.rating}
-						text={` ${product.numReviews} reviews`}
-						color="orange"
-					/>
-				</Card.Text>
+				<Card.Body>
+					<Link to={`/products/${product._id}`}>
+						<Card.Title as="div">
+							<strong>{product.name}</strong>
+						</Card.Title>
+					</Link>
 
-				<Card.Text as="h6"> &#8369;{product.price}</Card.Text>
-			</Card.Body>
-		</Card>
+					<Card.Text as="div">
+						<Rating
+							value={product.rating}
+							text={` ${product.numReviews} reviews`}
+							color="orange"
+						/>
+					</Card.Text>
+
+					<Card.Text as="h6"> &#8369;{product.price}</Card.Text>
+				</Card.Body>
+			</Card>
+		</div>
 	)
 }
 
