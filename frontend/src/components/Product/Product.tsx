a248f014e48a207338808bb22ee21a8a2d24c970
Van Darrell Ponce <vanponce91@gmail.com>
2020-10-28 18:53:36 +0800

    added pop up animation

--- frontend/src/components/Product/Product.tsx
+++ frontend/src/components/Product/Product.tsx
@@ -22,48 +22,77 @@ interface Props {
 const Product: React.FC<Props> = (props) => {
 	const { product } = props
 	const cardRef = useRef(null)
+	const cardName = useRef(null)
+	const cardRating = useRef(null)
+	const cardPrice = useRef(null)
+	const cardImage = useRef(null)
 
 	return (
 		<div
 			className="mycontainer"
 			onTouchMove={(e) => {
-				let xAxis = (window.innerWidth / 2 - e.touches[0].clientX) / 10
-				let yAxis = (window.innerHeight / 2 - e.touches[0].clientY) / 10
+				let xAxis = (window.innerWidth / 2 - e.touches[0].clientX) / 15
+				let yAxis = (window.innerHeight / 2 - e.touches[0].clientY) / 15
 				cardRef.current.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
 			}}
 			onTouchStart={(e) => {
 				cardRef.current.style.transition = 'none'
+				cardImage.current.style.transform = 'translateZ(150px)'
+				cardName.current.style.transform = 'translateZ(150px)'
+				cardRating.current.style.transform = 'translateZ(150px)'
+				cardPrice.current.style.transform = 'translateZ(150px)'
 			}}
 			onTouchEnd={(e) => {
+				cardImage.current.style.transform = 'translateZ(0px)'
+				cardName.current.style.transform = 'translateZ(0px)'
+				cardRating.current.style.transform = 'translateZ(0px)'
+				cardPrice.current.style.transform = 'translateZ(0px)'
+
 				cardRef.current.style.transition = 'all 0.5s ease'
 				cardRef.current.style.transform = `rotateY(0deg) rotateX(0deg)`
 			}}
 			onMouseMove={(e) => {
-				let xAxis = (window.innerWidth / 2 - e.pageX) / 15
-				let yAxis = (window.innerHeight / 2 - e.pageY) / 15
+				let xAxis = (window.innerWidth / 2 - e.pageX) / 30
+				let yAxis = (window.innerHeight / 2 - e.pageY) / 30
 				cardRef.current.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
 			}}
 			onMouseEnter={(e) => {
+				cardImage.current.style.transform = 'translateZ(200px)'
+				cardName.current.style.transform = 'translateZ(150px)'
+				cardRating.current.style.transform = 'translateZ(150px)'
+				cardPrice.current.style.transform = 'translateZ(150px)'
+
 				cardRef.current.style.transition = 'none'
 			}}
 			onMouseLeave={(e) => {
-				cardRef.current.style.transition = 'all 0.5s ease'
+				cardImage.current.style.transform = 'translateZ(0px)'
+				cardName.current.style.transform = 'translateZ(0px)'
+				cardRating.current.style.transform = 'translateZ(0px)'
+				cardPrice.current.style.transform = 'translateZ(0px)'
+
+				cardRef.current.style.transition = 'all 1s ease'
 				cardRef.current.style.transform = `rotateY(0deg) rotateX(0deg)`
 			}}
 		>
 			<Card className="my-1 p-1 rounded card product__card" ref={cardRef}>
 				<Link to={`/products/${product._id}`}>
-					<Card.Img src={product.image} variant="top" />
+					<Card ref={cardImage}>
+						<Card.Img src={product.image} variant="top" />
+					</Card>
 				</Link>
 
 				<Card.Body className="product__card__body">
 					<Link to={`/products/${product._id}`}>
-						<Card.Title as="div" className="product__card__title">
-							<strong>{product.name}</strong>
+						<Card.Title
+							as="div"
+							className="product__card__title"
+							ref={cardName}
+						>
+							<strong>{product.name}</strong>{' '}
 						</Card.Title>
 					</Link>
 
-					<Card.Text as="div">
+					<Card.Text as="div" ref={cardRating}>
 						<Rating
 							value={product.rating}
 							text={` ${product.numReviews} reviews`}
@@ -71,7 +100,9 @@ const Product: React.FC<Props> = (props) => {
 						/>
 					</Card.Text>
 
-					<Card.Text as="h6"> &#8369;{product.price}</Card.Text>
+					<Card.Text as="h6" ref={cardPrice}>
+						&#8369;{product.price}
+					</Card.Text>
 				</Card.Body>
 			</Card>
 		</div>
