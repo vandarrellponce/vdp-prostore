7bf44fac301009e74b12f25074989580df9fbe46
Van Darrell Ponce <vanponce91@gmail.com>
2020-10-31 18:22:30 +0800

    updated notif model

--- backend/models/notifiModel.js
+++ backend/models/notifiModel.js
@@ -2,10 +2,14 @@ import mongoose from 'mongoose'
 
 const notifSchema = mongoose.Schema(
 	{
-		target: { type: String, required: true }, // ex. user, admin, all
-		kind: { type: String, required: true }, // newOrder, updateOrder, newProduct
+		target: { type: String, required: true }, // ex. user, admin (all admin)
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+		},
 		message: { type: String, required: true },
 		payload: { type: String, required: true },
+		/* kind: { type: String, required: true }, // newOrder, updateOrder, newProduct */
 	},
 	{ timestamps: true }
 )
